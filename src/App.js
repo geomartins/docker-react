70849d8f739651b1e510904e8ae06ffd1dba9f1b
geomartins <martinsabiodun94@gmail.com>
2021-12-22 17:46:13 +0100

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload shit now ok yeah.
+          Edit <code>src/App.js</code> and save to reload shit now ok yeah. I changed this
         </p>
         <a
           className="App-link"
